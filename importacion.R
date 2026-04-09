a3f8f2377b82313639891f40baca2b5691d4d74a
Alejandra <uciraghrrg@gmail.com>
2026-04-09 11:16:39 -0300

    Add files via upload

---
+++ importacion.R
@@ -0,0 +1,5 @@
+# Importación de datos actualizados en el drive
+
+data <- read.csv2("Data/UC_IRAG_EST20892.csv",sep = ";", encoding = "latin1")
+
+agrupada <- read_xlsx("Data/agrupada excel 2024.xlsx")
