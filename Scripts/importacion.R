2633decd6bbe8d84ea13206598d44be6ccacf350
mercedespaz <pazmercedes.mp@gmail.com>
2026-04-16 12:36:44 -0300

    Agregado de periodo de análisis según plan.

--- Scripts/importacion.R
+++ Scripts/importacion.R
@@ -3,3 +3,19 @@
 data <- read.csv2("Data/UC_IRAG_EST20892.csv",sep = ";", encoding = "latin1")
 
 agrupada <- read_xlsx("Data/agrupada excel 2024.xlsx")
+
+#Cambié nombre de archivo
+agrupada_2026 <-data_agrupada_2026 <- read.csv("Data/AG_UC_IRAG_2026.csv", sep = ";", encoding = "latin1")
+
+
+# ===== PARÁMETROS TEMPORALES PARA EL ANÁLISIS ======
+
+#Defino parámetros temporales de análisis (establecidos en el plan)
+
+ANIO_MINIMO <- 2024
+
+SEMANA_MINIMA <- 18
+
+ANIO_MAXIMO <- 2026
+
+SEMANA_MAXIMA <- 8
