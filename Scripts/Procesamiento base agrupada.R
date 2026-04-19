2b3b21fd0747eac26d5638900dd10c9806fa127f
Alejandra Vera <uciraghrrg@gmail.com>
2026-04-19 13:43:07 -0300

    fallecidos, revisar

--- Scripts/Procesamiento base agrupada.R
+++ Scripts/Procesamiento base agrupada.R
@@ -133,6 +133,7 @@ tabla_resumen <- tabla_resumen %>% mutate(PROPORCION_IRAG = round(
 # 📊 Grafico interactivo proporcion de casos de IRAG e IRAGe entre los internados   
 #================================================================================
 
+ 
 curva_internaciones_irag <-highchart() %>%
   hc_chart(type= "column") %>%
   hc_plotOptions(column = list(stacking = "percent",
@@ -161,5 +162,32 @@ curva_internaciones_irag <-highchart() %>%
     name = "IRAGe",
     color = "#beaed4")
 
+# Total de fallecidos por todas las causas y total de irag e irag e
+# ESA TABLA SOLO MUESTRA LAS PROPORCIONES, PERO NO MUESTRA EL TOTAL DE FALLECIDOS QUE NO HAY CASOS DE IRAG E IRAG E
 
-curva_internaciones_irag 
+
+curva_fallecidos_irag <-highchart() %>%
+  hc_chart(type= "column") %>%
+  hc_plotOptions(column = list(stacking = "percent",
+                               pointPadding = 0.1,   
+                               groupPadding = 0.05,  
+                               borderWidth = 0)) %>%
+  hc_xAxis(
+    categories = tabla_resumen$SEPI, #categorías en eje X
+    title = list(text = "Año - Semana")) %>%  #título del eje X) 
+  hc_yAxis(
+    title = list(text = "Porcentaje de fallecidos"),
+    labels = list(format = "{value}%"),
+    max = 100) %>%
+  hc_credits(text = "Fuente: Elaboración propia en base a datos del SNVS 2.0", 
+             enabled = TRUE) %>% 
+  hc_add_series(
+    data = tabla_resumen$PROPORCION_FALLECIDOS,
+    name = "Otras causas",
+    color = "lightgrey") %>%
+  hc_add_series(
+    data = tabla_resumen$FALLECIDOS_IRAG,
+    name = "FALLECIDOS POR IRAG E IRAG EXTENDIDA",
+    color = "#7fc97f")
+
+curva_fallecidos_irag
