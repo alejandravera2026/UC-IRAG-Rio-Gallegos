7d50804ed0026340c9943bf75b40f4ae59272ba1
mercedespaz <pazmercedes.mp@gmail.com>
2026-04-23 15:59:17 -0300

    Observaciones cálculo de positividad

--- Scripts/Porcentaje de positividad.R
+++ Scripts/Porcentaje de positividad.R
@@ -1,10 +1,5 @@
 # Determinaciones positivas por se y por año
 
-ANIO_MINIMO <- 2024
-ANIO_MAXIMO <-2026
-SEMANA_MINIMA <-18
-SEMANA_MAXIMA <-8
-
 unique(data$INFLUENZA_FINAL)
 
 positividad_se <- data %>%
@@ -39,3 +34,90 @@ ggplot(positividad_se, aes(x = SEPI, y= PORCENTAJE_POSITIVIDAD, group = 1 )) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
+
+# Positividad por SE y virus
+
+#Selecciono variables
+
+positividad_se <- data %>% select(ANIO_MIN_INTERNACION,SEPI_MIN_INTERNACION,
+                                  VSR_FINAL,INFLUENZA_FINAL,COVID_19_FINAL)
+
+
+#Completo SE y creo variable SEPI
+positividad_se <- positividad_se %>% complete(ANIO_MIN_INTERNACION,
+                                              SEPI_MIN_INTERNACION = 1:52,
+                                              fill = list(n = 0)) %>%
+  mutate(SEPI= paste(ANIO_MIN_INTERNACION,"-",str_pad(SEPI_MIN_INTERNACION,2,pad= "0")))
+
+
+#Filtro según periodo de estudio
+
+positividad_se <- positividad_se %>%
+  filter(
+    
+    #Desde el inicio del periodo de análisis
+    (ANIO_MIN_INTERNACION > ANIO_MINIMO | 
+       (ANIO_MIN_INTERNACION == ANIO_MINIMO & SEPI_MIN_INTERNACION >= SEMANA_MINIMA)) &
+      
+      # Hasta el final del periodo de análisis
+      (ANIO_MIN_INTERNACION< ANIO_MAXIMO | 
+         (ANIO_MIN_INTERNACION == ANIO_MAXIMO & SEPI_MIN_INTERNACION <= SEMANA_MAXIMA))
+  )
+
+
+# Pasamos a formato largo (pivot longer)
+
+positividad_se <- positividad_se %>%
+  pivot_longer(cols = c(INFLUENZA_FINAL, COVID_19_FINAL, VSR_FINAL),
+               names_to = "Agente", values_to = "resultado") %>%
+  filter(resultado != "Sin resultado") %>%
+  mutate(Agente = case_when(
+    Agente == "INFLUENZA_FINAL" ~ "Influenza",
+    Agente == "COVID_19_FINAL"  ~ "SARS-CoV-2",
+    Agente == "VSR_FINAL"       ~ "VSR"
+  ))
+
+
+# Calculamos positividad por virus y semana
+
+positividad_se <- positividad_se %>%
+  group_by(SEPI, Agente) %>%
+  summarise(
+    ESTUDIADOS = n(),
+    POSITIVOS = sum(resultado != "Negativo"),
+    .groups = "drop"
+  ) %>%
+  mutate(POSITIVIDAD = round(POSITIVOS/ESTUDIADOS *100,1))
+
+
+#Pasamos a formato ancho (pivot wider) para gráfico interactivo
+positividad_se <- positividad_se %>% pivot_wider(names_from = Agente,
+                                                 values_from = POSITIVIDAD,
+                                                 values_fill = 0)
+
+
+#Grafico de líneas interactivo
+
+positividad_lineas <- highchart() %>%
+  hc_chart(type= "line") %>%
+  hc_xAxis(title = list(text = "SE - Año"),
+           categories = positividad_se$SEPI) %>%
+  hc_yAxis(
+    title = list(text = "Porcentaje de positividad"),
+    min = 0,
+    max = 100,
+    tickInterval = 10,
+    labels = list(format = "{value}%")
+  )  %>%
+  hc_add_series(name = "Influenza", 
+                data = positividad_se$Influenza,
+                color = "#7ac2a3") %>%
+  hc_add_series(name = "VSR", 
+                data = positividad_se$VSR,
+                color = "#C27E79" ) %>%
+  hc_add_series(name = "SARS-Cov-2", 
+                data = positividad_se$`SARS-CoV-2`,
+                color = "#ff7f0e" )
+
+
+positividad_lineas
