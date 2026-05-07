c5f733f676b4876cb3f8eec789b6e1ca06c4b163
epidemiohrrg26 <epidemio.hrrg26@gmail.com>
2026-05-07 11:11:50 -0300

    se filtra para eliminar caso invalidado

--- Scripts/3_limpieza.R
+++ Scripts/3_limpieza.R
@@ -2,3 +2,9 @@
 # SCRIPTS 3 - LIMPIEZA
 # UNIDAD CENTINELA DE INFECCIONES RESPIRATORIAS AGUDAS LIMPIEZA
 # ==================================================================
+
+
+data <- data %>%
+  filter(
+    CLASIFICACION_MANUAL != "Caso invalidado por epidemiología"
+  )
