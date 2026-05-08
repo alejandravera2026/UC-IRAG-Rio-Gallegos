92e7a58b7aedb40e0191671c930a37108d37de2f
Alejandra <uciraghrrg@gmail.com>
2026-05-08 09:43:33 -0300

    Se elimina la base creada que figura la positividad de virus, dejando higchcart que demuestra todos los virus

--- Scripts/Obj_3_Positividad_virus.R
+++ Scripts/Obj_3_Positividad_virus.R
@@ -3,52 +3,11 @@
 # SARS - Cov2, Influenza y VSR
 #=============================================
 
-#==============================================================================
-# Se arma una gráfica solamente el total de positivos para virus por semana y año
-#==============================================================================
-
-# Se crea una base de positividad por semana epidemiológica y por año
-
-# Se filtran los sin resultados y los negativos
-
-# Se crea un pivot longer 
-
 unique(data$INFLUENZA_FINAL)
-
-positividad_se <- data %>%
-  filter(INFLUENZA_FINAL != "Sin Resultado"| COVID_19_FINAL != "Sin resultado" | VSR_FINAL != "Sin resultado") %>%
-  pivot_longer(cols =  c(INFLUENZA_FINAL, COVID_19_FINAL, VSR_FINAL),
-               names_to = "Virus",
-               values_to = "Resultado") %>%
-  filter(Resultado != "Sin resultado") %>%
-  mutate(POSITIVO = case_when(Virus == "INFLUENZA_FINAL"& Resultado != "Negativo"~1,
-                              Virus %in% c("COVID_19_FINAL", "VSR_FINAL") & Resultado == "Positivo"~1,
-                              TRUE ~ 0)) %>%
-  group_by( ANIO_MIN_INTERNACION, SEPI_MIN_INTERNACION) %>%
-  summarise(DETERMINACIONES = n (),
-            POSITIVOS = sum(POSITIVO),
-            PORCENTAJE_POSITIVIDAD = round(POSITIVOS/DETERMINACIONES * 100.1),
-            .groups = "drop") %>%
-  filter(DETERMINACIONES >=5) %>%
-  mutate(SEPI= paste(ANIO_MIN_INTERNACION,"-", SEPI_MIN_INTERNACION))
-
-ggplot(positividad_se, aes(x = SEPI, y= PORCENTAJE_POSITIVIDAD, group = 1 )) +
-  geom_line(linewidth = 1, color ="#2C5") +
-  geom_point(size= 2, color = "#2C5") +
-  scale_y_continuous(limits = c(0,100),
-                     breaks = seq(0, 100, 10),
-                     labels = scales::label_percent(scale = 1)) +
-  scale_x_discrete(breaks = function(x) x[seq(1,length(x), by = 4)]) +
-  labs(title ="Porcentaje de positividad por SE y por año",
-       caption = "Fuente SNVS 2.0",
-       x = "Semana por semana y por año",
-       y = "% Positividad") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
 #===========================================================================
-# Se arma otro gráfico de positividad por SE y tipo de virus
+# Se arma  gráfico de positividad por SE y tipo de virus
 #===========================================================================
 
 #Se arma otra base y se selecciona las  variables a estudiar
