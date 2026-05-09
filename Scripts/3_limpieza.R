88faece6c85baef53cc0c49e1046ff8ba0865230
epidemiohrrg26 <epidemio.hrrg26@gmail.com>
2026-05-09 20:44:43 -0300

    ordené y corregí scrpt de limpieza y verifique la base final

--- Scripts/3_limpieza.R
+++ Scripts/3_limpieza.R
@@ -4,7 +4,7 @@
 # ===========================================================================
 
 
-# Elimina casos invalidados por epidemiología -----------------------------
+# 1- ELIMINO "Casos invalidados por epidemiología" ------------------------
 
 data <- data %>%
   filter(
@@ -12,50 +12,56 @@ data <- data %>%
   )
 
 
-# Completo semanas epidemiológicas y creo variable SEPI -------------------
+# 2- CREO VARIABLE SEPI ---------------------------------------------------
 
-data <- data %>% complete(ANIO_MIN_INTERNACION,
-                          SEPI_MIN_INTERNACION = 1:52,
-                          fill = list(n = 0)) %>%
-  mutate(SEPI= paste(ANIO_MIN_INTERNACION,"-",
-                     str_pad(SEPI_MIN_INTERNACION,2,pad= "0")
-                     )
-         )
-
-
-# Normalización de variables y filtrado temporal --------------------------
-
-data$ANIO_FECHA_MINIMA <- as.numeric (data$ANIO_MIN_INTERNACION)
-
-data$SEPI_FECHA_MINIMA <- as.numeric (data$SEPI_MIN_INTERNACION)
+data <- data %>%
+  mutate(
+    SEPI= paste(
+    ANIO_MIN_INTERNACION,
+    "-",
+    str_pad(SEPI_MIN_INTERNACION,2,pad=
+              "0")
+    )
+    )
 
 
-# Verifico si las variables estan como NUMÉRICAS --------------------------
-
-str(data$ANIO_MIN_INTERNACION)
-str(data$SEPI_MIN_INTERNACION)
-
-
-# Filtro período a analizar -----------------------------------------------
+# 3- FILTRO PERIODO A ANALIZAR  -------------------------------------------
 
 data <- data %>%
   filter(
-    (ANIO_MIN_INTERNACION > ANIO_MINIMO | 
-       (ANIO_MIN_INTERNACION == ANIO_MINIMO & 
-          SEPI_MIN_INTERNACION >= SEMANA_MINIMA)) &
-      (ANIO_MIN_INTERNACION< ANIO_MAXIMO | 
-         (ANIO_MIN_INTERNACION == ANIO_MAXIMO & 
-            SEPI_MIN_INTERNACION <= SEMANA_MAXIMA))
-  )
+    (ANIO_MIN_INTERNACION >
+       ANIO_MINIMO | 
+       (ANIO_MIN_INTERNACION == 
+          ANIO_MINIMO & 
+          SEPI_MIN_INTERNACION >= 
+          SEMANA_MINIMA)
+     ) &
+      (ANIO_MIN_INTERNACION< ANIO_MAXIMO |
+         (ANIO_MIN_INTERNACION == 
+            ANIO_MAXIMO & 
+            SEPI_MIN_INTERNACION <= 
+            SEMANA_MAXIMA)
+       )
+    )
 
-<<<<<<< HEAD
-=======
-str(data)
+
+# 4- CREO BASE ANALÍTICA PRINCIPAL LIMPIA ---------------------------------
+# data = base original trabajada
+# data_principal = base analítica limpia
+
+data_principal <- data
 
 
-# Creo una nueva base  ----------------------------------------------------
+# 5- CONTROL FINAL DE BASE ANALÍTICA --------------------------------------
 
-data_principal <- data%>%
-  select()
->>>>>>> 838b0e10e580757d8df62603df4f3225c44c84d0
+glimpse(data_principal)
+
+dim(data_principal)
+
+table(data_principal$CLASIFICACION_MANUAL)
+
+table(data_principal$ANIO_MIN_INTERNACION)
+
+str(data_principal)
+
 
