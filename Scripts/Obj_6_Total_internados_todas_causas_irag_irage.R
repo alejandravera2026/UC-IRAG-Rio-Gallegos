f6c5150529283cbcc6542868847dc2957ec6c184
Alejandra <uciraghrrg@gmail.com>
2026-05-09 14:15:35 -0300

    se intenta resolver los fallecidos y aun falta lo que se borro del total de internados por todas las causas y por irag/irage

--- Scripts/Obj_6_Total_internados_todas_causas_irag_irage.R
+++ Scripts/Obj_6_Total_internados_todas_causas_irag_irage.R
@@ -153,5 +153,29 @@ tabla_resumen <- tabla_resumen %>% mutate(
 
 tabla_resumen <- tabla_resumen %>% filter(PROPORCION_UCI_OTRAS_CAUSAS >= 0)
 
+#======================================================================================
+# GRAFICO TOTAL DE FALLECIDOS VS TOTAL POR IRAG E IRAG E
+#======================================================================================
+
+highchart() %>%
+  hc_chart(type = "column") %>%
+  hc_title (text = "Proporción de fallecidos por IRAG/IRAGe vs otras causas") %>%
+  hc_xAxis(categories = tabla_resumen$SEPI) %>%
+  hc_yAxis(
+    title = list(text = "Porcentaje"),
+    max = 100,
+    labels = list(format = "{value}%")) %>%
+  hc_plotOptions(
+    column = list(stacking = "normal", dataLabels = list(enabled = TRUE, format = "{y}%")))%>%
+  hc_add_series(
+    name = "Fallecidos IRAG/IRAGe",
+    data = tabla_resumen$PROPORCION_FALLECIDOS,
+    color = "#c62828") %>%
+  hc_add_series(
+    name ="Fallecidos otras causas",
+    data = tabla_resumen$PROPORCION_FALLECIDOS_OTRAS_CAUSAS,
+    color = "#90a4ae") %>%
+  hc_tooltip(shared = TRUE, valueSuffix ="%")
+
 
 
