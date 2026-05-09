a470fd5a64cdfa3f6fea3487f7eed4eb98a05341
epidemiohrrg26 <epidemio.hrrg26@gmail.com>
2026-05-09 11:37:55 -0300

    se continua con las tareas de limpieza de datos

--- Scripts/3_limpieza.R
+++ Scripts/3_limpieza.R
@@ -1,45 +1,60 @@
-# ==================================================================
+# ===========================================================================
 # SCRIPTS 3 - LIMPIEZA
 # UNIDAD CENTINELA DE INFECCIONES RESPIRATORIAS AGUDAS LIMPIEZA
-# ==================================================================
+# ===========================================================================
 
-#Elimina casos invalidados por epidemiología
+
+# Elimina casos invalidados por epidemiología -----------------------------
 
 data <- data %>%
   filter(
     CLASIFICACION_MANUAL != "Caso invalidado por epidemiología"
   )
 
-#=========================================================================
-# Completo semanas epidemiológicas y creo variable SEPI (une año con SE)
-# ========================================================================
+
+# Completo semanas epidemiológicas y creo variable SEPI -------------------
 
 data <- data %>% complete(ANIO_MIN_INTERNACION,
                           SEPI_MIN_INTERNACION = 1:52,
                           fill = list(n = 0)) %>%
-  mutate(SEPI= paste(ANIO_MIN_INTERNACION,"-",str_pad(SEPI_MIN_INTERNACION,2,pad= "0")))
+  mutate(SEPI= paste(ANIO_MIN_INTERNACION,"-",
+                     str_pad(SEPI_MIN_INTERNACION,2,pad= "0")
+                     )
+         )
 
 
-#===============================================
-# Aplico filtros respetanto periodo de análisis
-#==============================================
-
-#Se convierte a variable a numérica a las variables ANIO_MIN_INTERNACION Y SEPI_MIN_INTERNACION
+# Normalización de variables y filtrado temporal --------------------------
 
 data$ANIO_FECHA_MINIMA <- as.numeric (data$ANIO_MIN_INTERNACION)
 
 data$SEPI_FECHA_MINIMA <- as.numeric (data$SEPI_MIN_INTERNACION)
 
-#Filtra registros según lo definido en el plan de análisis
+
+# Verifico si las variables estan como NUMÉRICAS --------------------------
+
+str(data$ANIO_MIN_INTERNACION)
+str(data$SEPI_MIN_INTERNACION)
+
+
+# Filtro período a analizar -----------------------------------------------
 
 data <- data %>%
   filter(
-    
-    #Desde el inicio del periodo de análisis
     (ANIO_MIN_INTERNACION > ANIO_MINIMO | 
-       (ANIO_MIN_INTERNACION == ANIO_MINIMO & SEPI_MIN_INTERNACION >= SEMANA_MINIMA)) &
-      
-      # Hasta el final del periodo de análisis
+       (ANIO_MIN_INTERNACION == ANIO_MINIMO & 
+          SEPI_MIN_INTERNACION >= SEMANA_MINIMA)) &
       (ANIO_MIN_INTERNACION< ANIO_MAXIMO | 
-         (ANIO_MIN_INTERNACION == ANIO_MAXIMO & SEPI_MIN_INTERNACION <= SEMANA_MAXIMA))
+         (ANIO_MIN_INTERNACION == ANIO_MAXIMO & 
+            SEPI_MIN_INTERNACION <= SEMANA_MAXIMA))
   )
+
+str(data)
+
+
+# Selecciono variables no utilizadas en el plan  --------------------------
+
+
+
+
+
+
