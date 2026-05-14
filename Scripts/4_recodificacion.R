97b3fb445ba85680acec35e474fb232d9d2d4607
mercedespaz <pazmercedes.mp@gmail.com>
2026-05-14 11:12:36 -0300

    Aclaraciones sobre grupo etario y categorías de vacunación.

--- Scripts/4_recodificacion.R
+++ Scripts/4_recodificacion.R
@@ -10,6 +10,11 @@
 
 # 2- CREACIÓN DE GRUPOS ETARIOS SEGÚN VIGILANCIA UCIRAG -------------------
 
+# CREAR GRUPOS ETARIOS A PARTIR DE LA VARIABLE EDAD_UCIRAG 
+
+#Grupos: menor de 6 meses, 6 a 23 meses, 2 a 14, 15-64 y 65 años y más
+
+
 data <- data %>%
   mutate(
     grupo_etario = case_when(
@@ -44,10 +49,33 @@ table(data$grupo_etario, useNA = "ifany")
 prop.table(table(data$grupo_etario))*100
 
 
+# 4- CREACIÓN DE CATEGORÍAS PARA VACUNACIÓN -------------------
 
+# Aplicacion de vacuna VSR
 
+aplicacion_vsr <- c("SE 32","SE 33", "SE 34","SE 35", "SE 36", "SE DESCONOCIDA")
 
+# Categorías de vacunacion
 
+vacunado <- c("CONSTATADA", "REFERIDA")
 
+no_vacunado <- c("MADRE NO VACUNADA","NO VACUNADO")
 
+# APLICO NUEVAS CATEGORÍAS A LAS VACUNAS ANTIGRIPAL MATERNA Y VSR 
 
+data <- data %>% mutate (VAC_MATERNA_VSR = 
+                           case_when(VAC_VSR %in% aplicacion_vsr ~ "VACUNADA",
+                                     VAC_VSR %in% no_vacunado ~ "NO VACUNADA",
+                                      TRUE ~ "SIN DATO"),
+                                     
+                         VAC_ANTIGRIPAL_MATERNA = 
+                           case_when(VAC_ANTIGRIPAL_MATERNA == "MADRE NO VACUNADA" ~ "NO VACUNADA",
+                                     VAC_ANTIGRIPAL_MATERNA %in% vacunado ~ "VACUNADA",
+                                     TRUE ~ VAC_ANTIGRIPAL_MATERNA))
+
+# APLICO NUEVAS CATEGORÍAS A LAS VACUNAS ANTIGRIPAL  
+
+data <- data %>% mutate (VAC_ANTIGRIPAL = 
+                           case_when(VAC_ANTIGRIPAL == "NO VACUNADO" ~ "NO VACUNADO",
+                                     VAC_ANTIGRIPAL %in% vacunado ~ "VACUNADO",
+                                      TRUE ~ "SIN DATO"))
