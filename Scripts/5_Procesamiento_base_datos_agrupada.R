515697b8073a41262df3671fe7275fb40f944b94
epidemiohrrg26 <epidemio.hrrg26@gmail.com>
2026-05-19 23:20:01 -0300

    SE AGRGO TITULO Y SE ORENO SCRIPT

--- Scripts/5_Procesamiento_base_datos_agrupada.R
+++ Scripts/5_Procesamiento_base_datos_agrupada.R
@@ -1,156 +1,215 @@
-#Procesamiento base agrupada
+# ===========================================================================
+# SCRIPTS 5 - PROCESAMIENTO BASE AGRUPADA 
+# UNIDAD CENTINELA DE INFECCIONES RESPIRATORIAS AGUDAS 
+# ===========================================================================
 
-#Elimino primera fila de la base 
+
+# 1- ELIMINO PRIMERA FILA DE LA BASE  -------------------------------------
 
 agrupada <-agrupada [-1,]
 
 
-# Selecciono columnas a converir a numeric
+# 2- SELECCIONO COLUMNAS A CONVERTIR A NUMERIC  ---------------------------
 
 columnas_numeric <- agrupada %>%
-  select("ANIO", "SEMANA", "0 a 2 m":"Sin especificar") %>%
+  
+  select("ANIO", "SEMANA", 
+         "0 a 2 m":"Sin especificar") %>%
   names()
 
-#Transformo columnas a formato numérico
+
+# 3- TRANSFORMO COLUMNAS A FORMATO NUMÉRICO -------------------------------
+
 agrupada <- agrupada %>% 
-  mutate(across(all_of(columnas_numeric), ~ as.numeric(.x)))
+  
+  mutate(across(all_of(columnas_numeric),
+                ~ as.numeric(.x)
+                )
+         )
+
+# 4- SELECCIONO COLUMNAS DE INTERÉS ---------------------------------------
+
+columnas_eliminar <- c("PROVINCIA","FECHAREGISTROENCABEZADO",
+                       "ORIGEN","USUARIOREGISTROENCABEZADO")
 
 
-#Selecciono de columnas de interés para el análisis de la base agrupada
-
-columnas_eliminar <- c("PROVINCIA","FECHAREGISTROENCABEZADO","ORIGEN","USUARIOREGISTROENCABEZADO")
-
-#Selecciono eventos de interés para el análisis de la base agrupada
+# 5- SELECCIONO EVENTOS DE INTERÉS ----------------------------------------
 
 unique(agrupada$NOMBREEVENTOAGRP)
 
 agrupada <- agrupada %>% 
-  mutate(NOMBREEVENTOAGRP = str_replace_all(NOMBREEVENTOAGRP,
-                                                                        
-                            "Casos de IRAG EXTENDIDA entre los ingresados a UCI",
-                                                                        
-                            "Casos de IRAG extendida entre los ingresados a UCI"))
+  
+  mutate(NOMBREEVENTOAGRP = str_replace_all
+         (NOMBREEVENTOAGRP,
+           "Casos de IRAG EXTENDIDA entre los ingresados a UCI",
+           "Casos de IRAG extendida entre los ingresados a UCI")
+         )
 
-evento_agrupado <- c("Pacientes internados por todas las causas","Casos de IRAG entre los internados",
-                     "Casos de IRAG extendida entre los internados","Defunciones por IRAG","Defunciones por IRAG extendida",
-                     "Defunciones totales","Pacientes ingresados a UCI","Casos de IRAG entre los ingresados a UCI","Casos de IRAG extendida entre los ingresados a UCI")
+evento_agrupado <- c("Pacientes internados por todas las causas",
+                     "Casos de IRAG entre los internados",
+                     "Casos de IRAG extendida entre los internados",
+                     "Defunciones por IRAG","Defunciones por IRAG extendida",
+                     "Defunciones totales",
+                     "Pacientes ingresados a UCI",
+                     "Casos de IRAG entre los ingresados a UCI",
+                     "Casos de IRAG extendida entre los ingresados a UCI")
 
 
 
-#Aplico los filtros indicados
+# 6- APLICO LOS FILTROS INDICADOS -----------------------------------------
 
-agrupada <- agrupada %>% select(-any_of(columnas_eliminar)) %>%
+agrupada <- agrupada %>% 
+  
+  select(-any_of(columnas_eliminar)
+         ) %>%
+  
   filter(NOMBREEVENTOAGRP %in% evento_agrupado)
 
-#Pivoteo de datos a formato largo (longer) para agrupar por SE
 
-agrupada <- agrupada %>% pivot_longer(cols = "0 a 2 m":ncol(agrupada),
-                                      names_to = "GRUPO_ETARIO",
-                                      values_to = "CASOS") 
+# 7- PIVOTEO DE DATOS A FORMATO LARGO (LONGER) ----------------------------
+     #para agrupar por SE
 
-#Convierto CASOS a numeric para poder sumarlos
-agrupada <- agrupada %>% mutate(CASOS = as.numeric(CASOS))
+agrupada <- agrupada %>% 
+  
+  pivot_longer(cols = "0 a 2 m":ncol(agrupada),
+               names_to = "GRUPO_ETARIO",
+               values_to = "CASOS") 
 
-#================================================
-# ⚠️ BASE CARGA AGRUPADA A PARTIR DE SE 04 AÑO 2026
-#===============================================
 
-#Selecciono columnas de interés para el análisis
+# 8- CONVIERTO CASOS A NUMERIC PARA SUMAR  --------------------------------
 
-agrupada_2026 <- agrupada_2026 %>% select(ANIO,SEMANA,NOMBRE_EVENTO_AGRP,GRUPO,CANTIDAD)
+agrupada <- agrupada %>%
+  
+  mutate(CASOS = as.numeric(CASOS)
+         )
 
-# Aplico filtros respetando periodo de análisis (hasta semana 08 del 2026)
+# 9- B. AGRUPADA A PARTIR DE SE 04 AÑO 2026 -------------------------------
+     # Selecciono columnas de interés 
+
+agrupada_2026 <- agrupada_2026 %>% 
+  select(ANIO,SEMANA,NOMBRE_EVENTO_AGRP,GRUPO,CANTIDAD)
+
+
+# 10- APLICO FILTROS (HASTA SE 8 DEL 2026) --------------------------------
+
 agrupada_2026 <- agrupada_2026 %>%
-  filter(
-    
-    #Desde el inicio del periodo de análisis
+  
+  filter( # Desde el inicio del periodo de análisis
     (ANIO > ANIO_MINIMO | 
-       (ANIO == ANIO_MINIMO & SEMANA >= SEMANA_MINIMA)) &
+       (ANIO == ANIO_MINIMO & SEMANA >= SEMANA_MINIMA)
+     ) &
       
       # Hasta el final del periodo de análisis
       (ANIO < ANIO_MAXIMO | 
          (ANIO == ANIO_MAXIMO & SEMANA <= SEMANA_MAXIMA))
   )
 
+# 11- FILTRO EVENTOS DE INTERÉS -------------------------------------------
 
-# Filtro eventos de interés
+agrupada_2026 <- agrupada_2026 %>% 
+  
+  filter(NOMBRE_EVENTO_AGRP %in% evento_agrupado)
 
-agrupada_2026 <- agrupada_2026 %>% filter(NOMBRE_EVENTO_AGRP %in% evento_agrupado)
+  # Filtro grupo de edad
 
-#Filtro grupo de edad
+agrupada_2026 <- agrupada_2026 %>% 
+  
+  filter(GRUPO != "Todos los rangos")
 
-agrupada_2026 <- agrupada_2026 %>% filter(GRUPO != "Todos los rangos")
-
-#Unifico categorías de grupo de edad
+# Unifico categorías de grupo de edad
 
 agrupada_2026 <- agrupada_2026 %>%
-  mutate(GRUPO = str_replace_all(GRUPO, ">= a 75", ">= a 75 años"),
-         GRUPO = str_replace_all(GRUPO, "Edad sin esp.", "Sin especificar"))
+  
+  mutate(GRUPO = str_replace_all(GRUPO, 
+                                 ">= a 75", ">= a 75 años"),
+         GRUPO = str_replace_all(GRUPO, 
+                                 "Edad sin esp.", "Sin especificar")
+         )
 
-#Renombro columnas para que coincidan con el drive agrupado y poder unir bases de datos agrupadas
+# 12- RENOMBRO COLUMNAS  --------------------------------------------------
+ # para que coincidan con el drive agrupado y asi unir BD agrupadas
 
-agrupada_2026 <- agrupada_2026 %>% rename ("CASOS" = "CANTIDAD",
-                                           "GRUPO_ETARIO" = "GRUPO",
-                                           "NOMBREEVENTOAGRP" = "NOMBRE_EVENTO_AGRP")
-
-
-#Uno bases de datos: drive agrupado y exportación SNVS
-
-agrupada <- agrupada %>% rbind(agrupada_2026)
+agrupada_2026 <- agrupada_2026 %>% 
+  rename ("CASOS" = "CANTIDAD",
+          "GRUPO_ETARIO" = "GRUPO",
+          "NOMBREEVENTOAGRP" = "NOMBRE_EVENTO_AGRP")
 
 
-#===========================================
-#Agrupo tabla por semana, evento y año
-#============================================
+ # uno bases de datos: drive agrupado y exportación SNVS
 
-tabla_resumen <- agrupada %>% group_by(ANIO,SEMANA,NOMBREEVENTOAGRP) %>%
+agrupada <- agrupada %>% 
+  rbind(agrupada_2026)
+
+
+# 13- AGRUPO TABLA POR SE, EVENTO Y AÑO -----------------------------------
+
+tabla_resumen <- agrupada %>% 
+  
+  group_by(ANIO,SEMANA,NOMBREEVENTOAGRP) %>%
   summarise(CASOS = sum(CASOS,na.rm =T)) %>%
   ungroup()
 
+  # Creo SEPI (une año con SE) para etiquetar ejes
+tabla_resumen <- tabla_resumen %>% 
+  
+  mutate(SEPI = paste(ANIO,"-",SEMANA)
+         )
 
-#Creo SEPI (une año con SE) para etiquetar ejes
-tabla_resumen <- tabla_resumen %>% mutate(SEPI = paste(ANIO,"-",SEMANA))
+# 14- PASO BASE A FORMATO ANCHO (WIDER) -----------------------------------
+      # para calcular proporciones
 
-#===============================================================
-# Paso base a formato ancho (wider) para calcular proporciones
-#===============================================================
-
-tabla_resumen <- tabla_resumen %>% pivot_wider(names_from = NOMBREEVENTOAGRP,
-                                               values_from = CASOS)
+tabla_resumen <- tabla_resumen %>% 
+  pivot_wider(names_from = NOMBREEVENTOAGRP,
+              values_from = CASOS)
 
 
-#===============================================================
-# ⚠️ Calculo porporciones sobre la base agrupada
-#===============================================================
+# 15- CALCULO PROPROCIONES SOBRE BASE AGRUPADA ----------------------------
+      # Creo variable fallecidos por IRAG e IRAGe 
 
-# Creo variable fallecidos por IRAG e IRAGe 
+tabla_resumen <- tabla_resumen %>% 
+  
+  mutate(FALLECIDOS_IRAG = (`Defunciones por IRAG` + 
+                              `Defunciones por IRAG extendida`),
+         UCI_IRAG_IRAGE = (`Casos de IRAG entre los ingresados a UCI`+
+                          `Casos de IRAG extendida entre los ingresados a UCI`)
+         ) 
 
-tabla_resumen <- tabla_resumen %>% mutate(FALLECIDOS_IRAG = (`Defunciones por IRAG` + `Defunciones por IRAG extendida`),
-                                         UCI_IRAG_IRAGE = (`Casos de IRAG entre los ingresados a UCI`+`Casos de IRAG extendida entre los ingresados a UCI`)) # Agrego la columna ingresados a UCI por IRAG e IRAGE
-                                          
-# Proporcion IRAG e IRAGe sobre ingresos totales
+# 16- PROPORCIÓN IRAG E IRAGe / INGRESOS TOTALES  -------------------------
 
 colnames(tabla_resumen)
 
-tabla_resumen <- tabla_resumen %>% mutate(
-  PROPORCION_IRAG = round(
-  (`Casos de IRAG entre los internados`/`Pacientes internados por todas las causas`)*100,1),
+tabla_resumen <- tabla_resumen %>% 
+  mutate(PROPORCION_IRAG = round(
+    (`Casos de IRAG entre los internados`/
+       `Pacientes internados por todas las causas`)*100,1),
+    
   PROPORCION_IRAGE = round(
-    (`Casos de IRAG extendida entre los internados`/`Pacientes internados por todas las causas`)*100,1),
-  PROPORCION_INTERNADOS_OTRAS_CAUSAS = (100-(PROPORCION_IRAG + PROPORCION_IRAGE)),
+    (`Casos de IRAG extendida entre los internados`/
+       `Pacientes internados por todas las causas`)*100,1),
+  
+  PROPORCION_INTERNADOS_OTRAS_CAUSAS = (100-(PROPORCION_IRAG + 
+                                               PROPORCION_IRAGE)
+                                        ),
   PROPORCION_FALLECIDOS = round(
     (FALLECIDOS_IRAG/`Defunciones totales`)*100,1),
+  
   PROPORCION_FALLECIDOS_OTRAS_CAUSAS = (100 - PROPORCION_FALLECIDOS),
   IRAG_UCI = round(
-    (`Casos de IRAG entre los ingresados a UCI`/`Pacientes ingresados a UCI`)*100,1),
+    (`Casos de IRAG entre los ingresados a UCI`/
+       `Pacientes ingresados a UCI`)*100,1),
+  
   IRAGE_UCI = round(
-    (`Casos de IRAG extendida entre los ingresados a UCI`/`Pacientes ingresados a UCI`)*100,1),
-  PROPORCION_UCI_OTRAS_CAUSAS = (100-(IRAG_UCI + IRAGE_UCI))
+    (`Casos de IRAG extendida entre los ingresados a UCI`/
+       `Pacientes ingresados a UCI`)*100,1),
+  
+  PROPORCION_UCI_OTRAS_CAUSAS = (100-(IRAG_UCI + IRAGE_UCI)
+                                 )
 )
 
+# 13- PARA ELIMINAR DE LOS GRÁFICOS LAS SE CON ERRORES DE CARGA  ----------
 
-# Para eliminar de los gráficos las semanas con errores de carga
-
-tabla_resumen <- tabla_resumen %>% filter(PROPORCION_UCI_OTRAS_CAUSAS >= 0)
+tabla_resumen <- tabla_resumen %>% 
+  
+  filter(PROPORCION_UCI_OTRAS_CAUSAS >= 0)
 
 
